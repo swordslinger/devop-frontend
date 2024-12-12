477fd660ff5bd3a1b685587c39b9cdb361440d66
swordslinger <seanskelton40@gmail.com>
2024-12-12 00:50:36 +0000

    built very basic UI and can log user to screen, need to implement cypress tests and much better user information validation

---
+++ src/components/Register.vue
@@ -0,0 +1,59 @@
+/* eslint-disable */
+<template>
+  <Form @submit= "Register" :validation-schema="schema">
+    <div>
+      <label for = "username">Username</label>
+      <Field name = "username" type="text" />
+      <ErrorMessage name="username" />
+
+      <label for = "email">Email</label>
+      <Field name = "email" type="email" />
+      <ErrorMessage name="email" />
+
+      <label for = "password">Password</label>
+      <Field name = "password" type="password" />
+      <ErrorMessage name="password" />
+
+      <button :disabled="loading">
+        <span v-show="loading"></span>
+        Register
+      </button>
+
+
+    </div>
+  </Form>
+  /* eslint-disable */
+  <div v-if="message" :class="successful ? 'alert-success' : 'alert-danger'">
+    {{ message }}
+  </div>
+
+</template>
+/* eslint-disable */
+<script>
+/* eslint-disable */
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import * as yup from 'yup'; // For validation schema
+/* eslint-disable */
+export default {
+  /* eslint-disable */
+  components: {
+    Form,
+    Field,
+    ErrorMessage
+  },
+  data(){
+    return {
+      schema: yup.object().shape({
+        username: yup.string().required('Username is requried'),
+        password: yup.string().required('password is requried'),
+        email: yup.string().required('email is requried')
+      })
+    }
+  },
+  methods:{
+    Register(values){
+      console.log(values)
+    }
+  }
+}
+</script>
