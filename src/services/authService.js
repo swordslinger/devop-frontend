c2afcbf2ea8895ea997c2877082796c0bbb844a6
swordslinger <seanskelton40@gmail.com>
2025-03-03 15:13:22 +0000

    added production endpoint

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,13 @@
 // Imports.
 import axios from 'axios'
 
-// BASE URL for login,logout and register.
-const AUTH_ENDPOINT = 'http://localhost:3000/auth'
+// BASE URL for login,logout and register. (TEST)
+// const AUTH_ENDPOINT = 'http://localhost:3000/auth'
+
+// BASE URL for login,logout and register. (Production)
+const AUTH_ENDPOINT = 'http://k8s-default-devopfro-3395a783d4-103de2cc0dd57654.elb.eu-north-1.amazonaws.com '
+
+
 
 // Class for login, logout and register methods.
 class AuthService{
